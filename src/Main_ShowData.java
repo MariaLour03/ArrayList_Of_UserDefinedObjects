b7c19aa48d36134300f2ae6be9f6c62d924b2055
Maria Lourdusamy <maria.lourdusamy@gmail.com>
2024-12-10 21:49:56 -0600

    Commit GLAB - 303.11.3 - ArrayList of User-Defined Objects.

---
+++ src/Main_ShowData.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main_ShowData {
+    public static void main(String[] args) {
+
+        // instantiation to class AddDataToArrayList
+        AddDataToArrayList b = new AddDataToArrayList();
+
+        ArrayList<Book> mybooklist = b.bookdetails();
+
+        for (Book showValue : mybooklist) {
+            // ---- invoking getter method for getting Data---------
+            System.out.println(showValue.getNumber() + " " + showValue.getName() + " " + showValue.getCategory()
+                    + " " + showValue.getAuthor());
+        }
+    }
+}
+
+
+
